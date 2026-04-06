800f44ec89f8931739356621e1fbed82f2ad3376
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-07 01:49:15 +0900

    feat: metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import QueryProvider from "@/providers/QueryProvider";
+
+export const metadata: Metadata = {
+  title: "Rels",
+  description:
+    "광주소프트웨어마이스터고 학생 주도 강연 개설 및 신청 기반 릴레이 스터디 관리 서비스 Rels",
+};
 
 export default function RootLayout({
   children,
