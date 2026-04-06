ca85a1b7c0a7b6c9e5217f2f30e4330194eab70a
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-07 01:51:47 +0900

    fix: queryprovider 파일에 client 통합

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,14 +1,26 @@
-"use client";
-
-import { QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "@/lib/queryClient";
 
 export default function QueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 10,
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
