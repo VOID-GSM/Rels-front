ff8d1fd8d65bfca29574f6a5fa148f776a9a1c53
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-07 17:33:33 +0900

    feat: Plus svg 생성

---
+++ src/assets/svg/Plus.tsx
@@ -0,0 +1,16 @@
+export default function Plus() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.6667 8.6653H8.66666V12.6653H7.33333V8.6653H3.33333V7.33197H7.33333V3.33197H8.66666V7.33197H12.6667V8.6653Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
