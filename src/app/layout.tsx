377729a646bf2341dd9df1a063fa60be8fc04860
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-10 12:16:21 +0900

    refactor: header provier 밖으로 빼기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,8 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <QueryProvider>
-          <Header />
-          {children}
-        </QueryProvider>
+        <Header />
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
