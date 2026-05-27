8adbee255b7c60b31be2b3fb7c4df667ae17c45f
jyuuuuu0 <s25023@gsm.hs.kr>
2026-05-27 18:12:38 +0900

    마이페이지 강연 관련 export 추가

--- src/entities/lecture/index.ts
+++ src/entities/lecture/index.ts
@@ -1,11 +1,24 @@
-export type { LectureType, LectureStatusType, GradeCapacities, EnrollmentApplicant, LectureEnrollmentsType } from "./model/types";
+export type {
+  LectureType,
+  LectureStatusType,
+  GradeCapacities,
+  EnrollmentApplicant,
+  LectureEnrollmentsType,
+  MyEnrolledLecture,
+  MyCreatedLecture,
+  MyLectureEnrollmentsType,
+} from "./model/types";
 export { useGetLectures } from "./model/useGetLectures";
 export { useGetLecture } from "./model/useGetLecture";
+export { useGetMyLectureEnrollments } from "./model/useGetMyLectureEnrollments";
 export { useCreateLecture } from "./model/useCreateLecture";
 export { useUpdateLecture } from "./model/useUpdateLecture";
 export type { UpdateLectureData, UpdateLectureSettingsData } from "./model/useUpdateLecture";
 export { useDeleteLecture } from "./model/useDeleteLecture";
 export { useEnrollLecture } from "./model/useEnrollLecture";
-export { useCancelEnrollment } from "./model/useCancelEnrollment";
+export {
+  useCancelEnrollment,
+  useCancelEnrollmentById,
+} from "./model/useCancelEnrollment";
 export { useGetEnrollments } from "./model/useGetEnrollments";
 export { getDisplayLectureStatus } from "./model/status";
